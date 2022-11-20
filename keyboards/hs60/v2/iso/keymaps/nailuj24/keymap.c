31ed0884e64a7434ffa24532a9272ad103aa3b5b
Julian Aloofi <nailuj24@gmail.com>
2022-11-20 01:13:24 +0100

    Turn Right Alt into Alt

--- keyboards/hs60/v2/iso/keymaps/nailuj24/keymap.c
+++ keyboards/hs60/v2/iso/keymaps/nailuj24/keymap.c
@@ -44,14 +44,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,          KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,     KC_LBRC,  KC_RBRC,
     KC_LCTL,         KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS, KC_ENT,
     LSFT_T(JK_MSHF), KC_NUBS,  KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,           RSFT_T(JK_MSHF),
-    MO(_KEY_EMU),    KC_LALT,  KC_LGUI,                       KC_SPC,                          KC_NO,    KC_RALT,  MO(_FN), KC_RCTL),
+    MO(_KEY_EMU),    KC_LALT,  KC_LGUI,                       KC_SPC,                          KC_RGUI,  KC_RALT,  MO(_FN), KC_RCTL),
 
 [_BASE_WINDOWS] = LAYOUT_60_iso( /* Base - Windows ISO */
     KC_ESC,          KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,     KC_MINS,       KC_EQL,  KC_BSPC,
     KC_TAB,          KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,     KC_LBRC,       KC_RBRC,
     KC_LCTL,         KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,  KC_QUOT,       KC_NUHS, KC_ENT,
     LSFT_T(JK_WSHL), KC_NUBS,  KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,                RSFT_T(JK_WSHR),
-    MO(_KEY_EMU),    KC_LGUI,  KC_LALT,                       KC_SPC,                          KC_NO,    LT(_WIN_SPCL, KC_RGUI), MO(_FN), KC_RCTL),
+    MO(_KEY_EMU),    KC_LGUI,  KC_LALT,                       KC_SPC,                          KC_LALT,  LT(_WIN_SPCL, KC_RGUI), MO(_FN), KC_RCTL),
 
 [_GAMES] = LAYOUT_60_iso( /* Disable features that are annoying in Games */
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,
