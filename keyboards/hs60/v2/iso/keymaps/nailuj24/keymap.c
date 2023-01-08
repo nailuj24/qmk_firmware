f9052333695eecc622f76520eca321aea000ce5a
Julian Aloofi <nailuj24@gmail.com>
2023-01-08 17:48:51 +0100

    Move shift braces to different modifiers

--- keyboards/hs60/v2/iso/keymaps/nailuj24/keymap.c
+++ keyboards/hs60/v2/iso/keymaps/nailuj24/keymap.c
@@ -28,9 +28,7 @@
 #define _MORPH 6
 
 // QMK can only send basic keycodes via Mod-Tap, hijack some
-#define JK_MSHF KC_F22
-#define JK_WSHL KC_F23
-#define JK_WSHR KC_F24
+#define JK_SB KC_F24
 
 enum custom_keycodes {
   MACOS = SAFE_RANGE,
@@ -40,18 +38,18 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE_MACOS] = LAYOUT_60_iso( /* Base - macOS ISO */
-    KC_ESC,          KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,
-    KC_TAB,          KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,     KC_LBRC,  KC_RBRC,
-    KC_LCTL,         KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS, KC_ENT,
-    LSFT_T(JK_MSHF), KC_NUBS,  KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,           RSFT_T(JK_MSHF),
-    MO(_KEY_EMU),    KC_LALT,  KC_LGUI,                       KC_SPC,                          KC_RGUI,  KC_RALT,  MO(_FN), KC_RCTL),
+    KC_ESC,              KC_1,     KC_2,     KC_3,  KC_4,  KC_5, KC_6,    KC_7,  KC_8,  KC_9,     KC_0,                 KC_MINS,  KC_EQL,     KC_BSPC,
+    KC_TAB,              KC_Q,     KC_W,     KC_E,  KC_R,  KC_T, KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,                 KC_LBRC,  KC_RBRC,
+    KC_LCTL,             KC_A,     KC_S,     KC_D,  KC_F,  KC_G, KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,              KC_QUOT,  KC_NUHS,    KC_ENT,
+    MT(MOD_LSFT, JK_SB), KC_NUBS,  KC_Z,     KC_X,  KC_C,  KC_V, KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,               KC_SLSH,              MT(MOD_RSFT, JK_SB),
+    LT(_KEY_EMU, JK_SB), KC_LALT,  MT(MOD_LGUI, JK_SB),          KC_SPC,                          MT(MOD_RGUI, JK_SB),  KC_RALT,  MO(_FN),    MT(MOD_RCTL, JK_SB)),
 
 [_BASE_WINDOWS] = LAYOUT_60_iso( /* Base - Windows ISO */
-    KC_ESC,          KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,     KC_MINS,       KC_EQL,  KC_BSPC,
-    KC_TAB,          KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,     KC_LBRC,       KC_RBRC,
-    KC_LCTL,         KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,  KC_QUOT,       KC_NUHS, KC_ENT,
-    LSFT_T(JK_WSHL), KC_NUBS,  KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,                RSFT_T(JK_WSHR),
-    MO(_KEY_EMU),    KC_LGUI,  KC_LALT,                       KC_SPC,                          KC_LALT,  LT(_WIN_SPCL, KC_RGUI), MO(_FN), KC_RCTL),
+    KC_ESC,              KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,                 KC_MINS,                KC_EQL,  KC_BSPC,
+    KC_TAB,              KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,                 KC_LBRC,                KC_RBRC,
+    KC_LCTL,             KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,              KC_QUOT,                KC_NUHS, KC_ENT,
+    MT(MOD_LSFT, JK_SB), KC_NUBS,  KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,               KC_SLSH,                         MT(MOD_RSFT, JK_SB),
+    LT(_KEY_EMU, JK_SB), KC_LGUI,  MT(MOD_LALT, JK_SB),           KC_SPC,                          MT(MOD_RALT, JK_SB),  LT(_WIN_SPCL, KC_RGUI), MO(_FN), MT(MOD_RCTL, JK_SB)),
 
 [_GAMES] = LAYOUT_60_iso( /* Disable features that are annoying in Games */
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -64,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MUTE, KC_VOLD,  KC_VOLU,  DM_REC1,  DM_PLY1,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPRV,
     KC_TRNS, KC_TRNS,  WINDOWS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  QK_BOOT,
     KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TG(_GAMES), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPLY,
-    KC_TRNS, EF_DEC,   EF_INC,   KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  MACOS,    KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_MNXT,
+    KC_TRNS, EF_DEC,   EF_INC,   ES_DEC,   ES_INC,   BR_DEC,     BR_INC,   KC_TRNS,  MACOS,    KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_MNXT,
     KC_TRNS, KC_TRNS,  KC_TRNS,                                  KC_TRNS,                                KC_NO,    KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [_KEY_EMU] = LAYOUT_60_iso( /* Key Emulation - Fn2 */
@@ -78,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  RALT(KC_8), RALT(KC_9),  RALT(KC_NUBS),  RALT(KC_7)/*8*/, RALT(KC_0), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS,             KC_TRNS,  KC_TRNS,  RALT(KC_E), KC_TRNS,  KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,         RALT(KC_Q), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    LT(_MORPH, JK_WSHL), KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,     RALT(KC_RBRC),  KC_TRNS,         KC_TRNS,    KC_TRNS,  KC_TRNS,            LT(_MORPH, JK_WSHR),
+    KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,     RALT(KC_RBRC),  KC_TRNS,         KC_TRNS,    KC_TRNS,  KC_TRNS,            KC_TRNS,
     KC_TRNS,             KC_TRNS,  KC_TRNS,                                    KC_TRNS,                                                  KC_RALT,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [_MORPH] = LAYOUT_60_iso( /* QMK does not have Mod-Morph, emulate with a layer */
@@ -122,76 +120,58 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed)
       set_single_persistent_default_layer(_BASE_WINDOWS);
     return false;
-    // Mac Shift Braces Tap
-  case LSFT_T(JK_MSHF):
+    // Shift Braces Tap
+   case MT(MOD_LSFT, JK_SB):
+     if (record->tap.count && record->event.pressed) {
+       tap_code16(S(KC_8));
+       return false;
+     }
+     break;
+   case MT(MOD_RSFT, JK_SB):
+     if (record->tap.count && record->event.pressed) {
+       tap_code16(S(KC_9));
+       return false;
+     }
+     break;
+  case MT(MOD_LALT, JK_SB):
     if (record->tap.count && record->event.pressed) {
-      clear_mods();
-      if (_mods & MOD_MASK_CTRL)
-        tap_code16(RALT(KC_8));
-      else if (_mods & MOD_BIT(KC_RALT))
-        tap_code16(RALT(KC_5));
-      else
-        tap_code16(S(KC_8));
-      set_mods(_mods);
-      return false;
-    }
-    break;
-  case RSFT_T(JK_MSHF):
-    if (record->tap.count && record->event.pressed) {
-      if (_mods & MOD_MASK_SHIFT)
-        process_record_user(LSFT_T(JK_MSHF), record);
-      clear_mods();
-      if (_mods & MOD_MASK_CTRL)
-        tap_code16(RALT(KC_9));
-      else if (_mods & MOD_BIT(KC_RALT))
-        tap_code16(RALT(KC_6));
-      else
-        tap_code16(S(KC_9));
-      set_mods(_mods);
-      return false;
-    }
-    break;
-    // Windows Shift Braces Tap
-  case LSFT_T(JK_WSHL):
-    if (record->tap.count && record->event.pressed) {
-      clear_mods();
-      if (_mods & MOD_MASK_CTRL)
-        tap_code16(RALT(KC_7));
-      else
-        tap_code16(S(KC_8));
-      set_mods(_mods);
-      return false;
-    }
-    break;
-  case LT(_MORPH, JK_WSHL):
-    // HOLD_ON_OTHER_KEY_PRESS does not apply to LT(), so check for interrupt manually
-    if ((record->tap.interrupted) || (record->tap.count && record->event.pressed)) {
-      clear_mods();
       tap_code16(RALT(KC_8));
-      set_mods(_mods);
       return false;
     }
     break;
-  case RSFT_T(JK_WSHR):
+  case MT(MOD_RALT, JK_SB):
     if (record->tap.count && record->event.pressed) {
-      if (_mods & MOD_MASK_SHIFT)
-        process_record_user(LSFT_T(JK_WSHL), record);
-      clear_mods();
-      if (_mods & MOD_MASK_CTRL)
-        tap_code16(RALT(KC_0));
-      else
-        tap_code16(S(KC_9));
-      set_mods(_mods);
-      return false;
-    }
-    break;
-  case LT(_MORPH, JK_WSHR):
-    if (record->tap.count && record->event.pressed) {
-      if (IS_LAYER_ON(_MORPH))
-        process_record_user(LT(_MORPH, JK_WSHL), record);
-      clear_mods();
       tap_code16(RALT(KC_9));
-      set_mods(_mods);
+      return false;
+    }
+    break;
+  case MT(MOD_LGUI, JK_SB):
+    if (record->tap.count && record->event.pressed) {
+      tap_code16(RALT(KC_5));
+      return false;
+    }
+    break;
+  case MT(MOD_RGUI, JK_SB):
+    if (record->tap.count && record->event.pressed) {
+      tap_code16(RALT(KC_6));
+      return false;
+    }
+    break;
+  case LT(_KEY_EMU, JK_SB):
+    if (record->tap.count && record->event.pressed) {
+      if (IS_LAYER_ON_STATE(default_layer_state, _BASE_MACOS))
+        tap_code16(RALT(KC_8));
+      else
+        tap_code16(RALT(KC_7));
+      return false;
+    }
+    break;
+  case MT(MOD_RCTL, JK_SB):
+    if (record->tap.count && record->event.pressed) {
+      if (IS_LAYER_ON_STATE(default_layer_state, _BASE_MACOS))
+        tap_code16(RALT(KC_9));
+      else
+        tap_code16(RALT(KC_0));
       return false;
     }
     break;
